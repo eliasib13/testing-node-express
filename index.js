c0bc805025cc032f12af108fb7f551ded09e99c5
Eliasib García <eliasib.lv.12@gmail.com>
2017-08-20 20:04:55 +0100

    Using router

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const app = express();
 // Adding environment variables
 require('dotenv').config();
 
-app.get('/', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.BASE_URL || '*');
-  res.send('Hello World!')
-});
+// Loading router
+const router = require('./router');
+app.use('/', router);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
